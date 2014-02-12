d138c2954bd6e63fbcf04722835ff9582043938b
Thomas DUFOUR <thomas.dufour@condorcet93.fr>
2014-02-12 23:14:18 +0100

    Message

--- app.js
+++ app.js
@@ -4,12 +4,16 @@
  */
 
 var express = require('express');
+var fs = require('fs');
+var thumb = require('node-thumbnail').thumb;
 var routes = require('./routes');
 var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
 
 var app = express();
+var dossier_diapos = path.join(__dirname, 'public/slices');
+var dossier_miniatures = path.join(__dirname, 'public/thumb-slices');
 
 // all environments
 app.set('port', process.env.PORT || 3000);
@@ -30,9 +34,54 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
 app.get('/users', user.list);
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-});
+// Message de bienvenue
+console.log('Serveur imprimante 3D - Lycée Condorcet');
+
+// Démarrage
+main();
+
+// Récupération des vignettes des diapos par lecture du dossier
+function main() {
+    fs.readdir(dossier_miniatures, function(err1, fichiersThumb) {
+        if (err1) {
+            console.log("> les miniatures n'existent pas, création automatique en cours...");
+            // Création du dossier thumb-slices
+            fs.mkdirSync(dossier_miniatures);
+            // Création des vignettes si elle n'existent pas
+            thumb({
+                source : dossier_diapos,
+                destination : dossier_miniatures,
+                concurrency : 4,
+                width : 40
+            }, function() {
+                console.log('> ...miniatures créées !');
+                main(); // on rappelle la fonction principale pour rescanner le dossier des diapos
+            });
+        } else {
+            fs.readdir(dossier_diapos, function(err2, fichiersDiapos) {
+
+                // Template de la page principale (avec le module ejs)
+                app.get('/', function(req, res) {
+                    res.render('index', {
+                        title : 'Imprimante 3D - Lycée Condorcet / Montreuil',
+                        thumbFichiers : fichiersThumb, // on transmets le tableau des vignettes en paramètres afin de les afficher
+                        diaposFichiers : fichiersDiapos // ainsi que le tableau des diapos pour les afficher une à une
+                    });
+                });
+
+                // Appel d'une diapo par passage de paramètre
+                app.get('/diapo/:num_diapo', function(req, res) {
+                    res.send(fichiersDiapos[req.params.num_diapo]);
+                });
+
+                http.createServer(app).listen(app.get('port'), function(){
+                    console.log('> ' + fichiersThumb.length + " vignettes lues");
+                    console.log('> Lancer http://localhost:' + app.get('port') + ' en mode plein écran ');
+                });
+            });
+        }
+    });
+}
+
